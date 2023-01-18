31e0155b41549c30d78354054f7e42ef45705a8a
thodd2209048 <114908027+thodd2209048@users.noreply.github.com>
2023-01-18 20:41:24 +0700

    Todolist

--- src/App.js
+++ src/App.js
@@ -1,42 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+import TodoFeature from './features/Todo';
 
 function App() {
-  const name = 'Tho';
-  const age = 18;
-  const isMale = true;
-  const student = {
-    name: 'Easy Frontend'
-  };
-  const colorList = ['red', 'green', 'blue'];
+
   return (
     <div className='App'>
-      <header className='App-header'>
-      <img src={logo} className='App-logo' alt='logo'/>
-        <p>
-        Thích thì nhích tới đích.
-        </p>
-        <p>
-            Xin chao {name} - {age} - {isMale? 'Male' : 'Female'}
-        </p>
-
-        {isMale? <p>Male</p> : <p>Female</p>}
-        {isMale && <p>Male</p>}
-        {isMale &&
-        <>
-          <p>Warrior</p>
-          <p>Female</p>
-          <p>Female</p>
-        </>
-        }
-        <p>{student.name}</p>
-        <ul>
-          {colorList.map(color => (
-            <li>{color}</li>
-          ))}
-        </ul>
-      </header>
+      <TodoFeature />
     </div>
   );
 }
