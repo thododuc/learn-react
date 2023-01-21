bff27b6e130c5c47248afc0936a17599b36c98a4
thodd2209048 <114908027+thodd2209048@users.noreply.github.com>
2023-01-21 22:00:27 +0700

    learn Route

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import TodoFeature from './features/Todo';
 // import logo from './logo.svg';
+import AlbumFeature from './features/Album';
 
 function App() {
 
   return (
     <div className='App'>
-      <TodoFeature />
-      {/* <AlbumFeature/>
-      <ColorBox/>
-      <Counter/> */}
+      <Routes>
+        <Route path="/" element={<div>Home Page</div>} />
+        <Route path="/todos" element={<TodoFeature/>}/>
+        <Route path="/albums" element={<AlbumFeature/>}/>
+      </Routes>
     </div>
   );
 }
