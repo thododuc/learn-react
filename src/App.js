aa6d9f0f12628c140f9edab8ad33d092b4d60022
thodd2209048 <114908027+thodd2209048@users.noreply.github.com>
2023-01-23 02:45:03 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link, NavLink, Route, Routes } from 'react-router-dom';
+import { Link, Navigate, NavLink, Route, Routes } from 'react-router-dom';
 import TodoFeature from './features/Todo';
 // import logo from './logo.svg';
 import AlbumFeature from './features/Album';
@@ -8,12 +8,11 @@ function App() {
 
   return (
     <div className='App'>
-      <p><Link to='/todos'>Todo</Link></p>
-      <p><Link to='/albums'>Album</Link></p>
-
       <p><NavLink to='/todos'>Todo</NavLink></p>
       <p><NavLink to='/albums'>Album</NavLink></p>
       <Routes>
+        
+
         <Route path="/" element={<div></div>} />
         <Route path="/todos" element={<TodoFeature/>}/>
         <Route path="/albums" element={<AlbumFeature/>}/>
