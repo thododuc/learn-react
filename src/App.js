b0e8b4c374045bfcbb98490804ddf1faa6f76e5a
thodd2209048 <114908027+thodd2209048@users.noreply.github.com>
2023-01-23 19:50:05 +0700

    not found

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { Link, Navigate, NavLink, Route, Routes } from 'react-router-dom';
 import TodoFeature from './features/Todo';
 // import logo from './logo.svg';
 import AlbumFeature from './features/Album';
+import NotFound from './components/NotFound';
 
 function App() {
 
   return (
     <div className='App'>
+      <p><NavLink to='/todos'>Todo</NavLink></p>
       <p><NavLink to='/albums'>Album</NavLink></p>
       <Routes>
         
@@ -16,6 +18,8 @@ function App() {
         <Route path="/" element={<div></div>}/>
         <Route path="/todos/*" element={<TodoFeature/>}/>
         <Route path="/albums" element={<AlbumFeature/>}/>
+
+        <Route path="*" element={<NotFound/>}/>
       </Routes>
     </div>
   );
--- src/features/Todo/index.jsx
+++ src/features/Todo/index.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { Link, Route, Routes } from 'react-router-dom';
+import NotFound from '../../components/NotFound';
 // import PropTypes from 'prop-types';
 import TodoList from './components/TodoList';
 import DetailPage from './pages/DetailPage';
@@ -12,12 +13,11 @@ TodoFeature.propTypes = {
 function TodoFeature(props) {
   return (
     <div>
-      <p><Link to='todos'>List Page</Link></p>
       <p><Link to='123'>Detail Page</Link></p>
       <Routes>
         <Route path='/' element={<ListPage/>}/>
         <Route path=':todoId' element={<DetailPage/>}/>
-
+        <Route path='*' element={<NotFound/>}/>
       </Routes>
     </div>
   );
